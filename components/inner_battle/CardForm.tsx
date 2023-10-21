076d69ca3b9663a2af7f06e956675cb62c5cdbff
jack tang <jack.tangth@gmail.com>
2023-10-21 20:59:48 +0800

    build UI logic for the form

--- components/inner_battle/CardForm.tsx
+++ components/inner_battle/CardForm.tsx
@@ -1,32 +1,94 @@
 "use client";
 import { addCard } from "@/app/inner_battle/action";
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
 function CardForm() {
-  const [toggle, setToggle] = useState(false);
+  const [focusOnForm, setFocusOnForm] = useState(false);
+  const [textAreaContent, setTextAreaContent] = useState("");
+  const [textAreaTitle, setTextAreaTitle] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
+  const handleTextareaSize = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const target = e.target as HTMLTextAreaElement;
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = `${target.scrollHeight}px`;
+    }
+  };
+  const handleTextareaInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const target = e.target as HTMLTextAreaElement;
+    const value = target.value;
+
+    switch (target.name) {
+      case "content":
+        setTextAreaContent(value);
+        break;
+      case "title":
+        setTextAreaTitle(value);
+
+        break;
+      default:
+        console.error("unknown target name");
+        break;
+    }
+  };
+  const textareaOnChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    handleTextareaSize(e);
+    handleTextareaInput(e);
+  };
+  const formMissedFoucs = () => {
+    if (textAreaContent || textAreaTitle) {
+      formRef.current?.requestSubmit();
+      setTextAreaContent("")
+      setTextAreaTitle("")
+    }
+    setFocusOnForm(false);
+  };
   const Form = () => {
     return (
-      <main className="">
-        <form method="post" action={addCard}>
-          {" "}
-          <label htmlFor="">
-            Title
-            <input type="text" name="title" />
-          </label>
-          <label htmlFor="">
-            Content
-            <input type="text" name="content" />
-          </label>
-          <button type="submit">Send</button>
-        </form>
-      </main>
+      <div>
+        <main>
+          <form ref={formRef} action={addCard}>
+            {" "}
+            {focusOnForm ? (
+              <label htmlFor="">
+                Title
+                <textarea
+                  className="resize-none"
+                  name="title"
+                  value={textAreaTitle}
+                  onChange={(e) => textareaOnChangeHandler(e)}
+                />
+              </label>
+            ) : (
+              <></>
+            )}
+            <label htmlFor="">
+              Content
+              <textarea
+                className="resize-none"
+                value={textAreaContent}
+                ref={textAreaRef}
+                name="content"
+                contentEditable={true}
+                onClick={() => setFocusOnForm(true)}
+                onChange={(e) => textareaOnChangeHandler(e)}
+                autoFocus={focusOnForm}
+              />
+            </label>
+            <button type="submit">Send</button>
+          </form>
+        </main>
+        {focusOnForm ? (
+          <div
+            className="fixed h-screen w-screen"
+            onClick={formMissedFoucs}
+          ></div>
+        ) : (
+          <></>
+        )}
+      </div>
     );
   };
-  return (
-    <div>
-      <button onClick={() => setToggle(!toggle)}>Add</button>
-      {toggle ? <Form /> : <></>}
-    </div>
-  );
+  return <Form />;
 }
 export default CardForm;
