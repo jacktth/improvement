ee063e08e202991cb34a2ac8688908bc49072c40
jack tang <jack.tangth@gmail.com>
2023-10-24 21:16:26 +0800

    add clear search text function

--- components/note/SearchBar.tsx
+++ components/note/SearchBar.tsx
@@ -2,18 +2,28 @@
 import { searchCardsWithInputTextACtion } from "@/app/note/action";
 import { increment, addSearchedCard } from "@/app/note/noteSlice";
 import { ICard, ICardAfterParsed } from "@/models/Card";
+import { useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 export type DocCardId = {
-    _id: string
-}
+  _id: string;
+};
 function SearchBar() {
+  const inputRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
-  const search = async(forData: FormData) => {
+  const search = async (forData: FormData) => {
     const res = await searchCardsWithInputTextACtion(forData);
-    const parsedObjectRes = Object.values<ICardAfterParsed>(JSON.parse(JSON.stringify(await res)));
-  
-    dispatch(addSearchedCard(parsedObjectRes))
+    const parsedObjectRes = Object.values<ICardAfterParsed>(
+      JSON.parse(JSON.stringify(await res))
+    );
+
+    dispatch(addSearchedCard(parsedObjectRes));
     console.log(parsedObjectRes);
+  };
+  const clearButtonHandler = () => {
+    if (inputRef.current) {
+      inputRef.current.value = "";
+      dispatch(addSearchedCard([]))
+    }
   };
   return (
     <>
@@ -23,7 +33,11 @@ function SearchBar() {
           title="Search"
           name="text"
           type="text"
+          ref={inputRef}
         />
+        <button type="button" onClick={clearButtonHandler}>
+          X
+        </button>
       </form>
     </>
   );
