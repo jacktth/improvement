0daf83a99d40e94cc5874d7bdd85ebe4a891e29b
jack tang <jack.tangth@gmail.com>
2023-11-08 18:00:43 +0800

    set home page via next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
     experimental:{
         serverComponentsExternalPackages:["mongoose"],
         serverActions: true,
-    }
+    },
+    exportPathMap:async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          '/': { page: '/note' },
+
+        }
+      },
 }
 
 module.exports = nextConfig
